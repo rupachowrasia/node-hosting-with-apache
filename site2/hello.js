7cf106a7a5633773c7f64704c49e257c092e65de
rupachowrasia <rupa.chowrasia@gmail.com>
2017-07-17 18:53:24 +0530

    Forgot to change port for second site

--- site2/hello.js
+++ site2/hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    response.end('Node.js is running form site2.\n');
-}).listen(8080);
+}).listen(3000);
 console.log('Server running at http://127.0.0.1:3000/');
